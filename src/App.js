12fc51d38d6ef98d8c83bf62e5997644cd2f201b
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-25 01:07:34 +0000

    Adding data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import babyNames from "./babyNamesData.json";
 import "./App.css";
+import Name from "./components/Name";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {babyNames.map((baby, index) => {
+        return <Name key={index} name={baby.name} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Name.js
@@ -0,0 +1,6 @@
+import React from "react";
+const Name = (props) => {
+  return <div className={props.class}>{props.name}</div>;
+};
+
+export default Name;
