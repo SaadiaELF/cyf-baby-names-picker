f0bcf18d443455ed4117fa3de4cbb6f261100026
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-25 01:30:13 +0000

    Sorted names

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import babyNames from "./babyNamesData.json";
 import "./App.css";
 import Name from "./components/Name";
 
+const sortedNames = babyNames.sort((a, b) => {
+  let nA = a.name.toLowerCase();
+  let nB = b.name.toLowerCase();
+
+  if (nA < nB) {
+    return -1;
+  }
+  if (nA > nB) {
+    return 1;
+  }
+  return 0;
+});
+
 function App() {
   return (
     <div className="App">
       <div className="names-container">
-        {babyNames.map((baby, index) => {
+        {sortedNames.map((baby, index) => {
           return (
             <Name key={index} name={baby.name} class={baby.sex + "-name"} />
           );
